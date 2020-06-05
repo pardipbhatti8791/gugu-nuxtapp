f80edddb8ff3c474245bf7248537e24a2ae0c28a
Pardip Bhatti <pardipbhatti8791@gmail.com>
2020-06-05 15:46:33 +0530

    nuxt world

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import EventCard from '~/components/EventCard'
 export default {
+  components: {
+    EventCard
+  },
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch('events/fetchEvents')
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time, please try again'
+      })
+    }
+  },
+  computed: mapState({
+    events: (state) => state.events.events
+  }),
   head() {
     return {
       title: 'Event Listing'
